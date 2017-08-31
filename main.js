8c3cfa74b3c0bd66cea92a82da3486a1ad4e41bc
EngAhmadGamal <31404631+EngAhmadGamal@users.noreply.github.com>
2017-08-31 12:06:06 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
  * Created by Ahmad on 1/07/2017.
  */
 var myName = prompt(' Please Enter Your Name || من فضلك أكتب اسمك ');
-document.write('<h3 align="center" style="color:red;">Move Your Mouse On Your Name || حرك الماوس على اسمك</h3>');
+document.write('<h3 align="center" style="color:red;">Move Your Mouse On The Name || حرك الماوس على اسمك</h3>');
 var red = [0, 100, 63];
 var orange = [40, 100, 60];
 var green = [75, 100, 40];
